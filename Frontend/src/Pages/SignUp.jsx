b4affb65166eba460b84f7dec166dd51c59fd3c2
Ankit0886 <ankitsinghchouhan0735@gmail.com>
2026-01-05 23:35:40 +0530

    Update SignUp.jsx

--- Frontend/src/Pages/SignUp.jsx
+++ Frontend/src/Pages/SignUp.jsx
@@ -38,36 +38,37 @@ function SignUp ()  {
 
   }
   return (
-    <div className='w-full h-[100vh] bg-cover flex justify-center items-center' 
+    <div className='w-full min-h-screen bg-[center_top] sm:bg-center bg-cover bg-no-repeat flex justify-center items-center px-3 sm:px-0' 
     style={{backgroundImage:`url(${bg})`}}> 
 
-    <form className='w-[90%] h-[600px] max-w-[500px] bg-[#0000003d] backdrop-blur-md shadow-md 
-    shadow-black rounded-md flex flex-col justify-center items-center gap-[20px] px-[20px]' onSubmit={handleSignup}>
+    <form className='w-[90%] h-[600px] max-w-[500px] h-auto sm:h-[600px] bg-[#0000003d] backdrop-blur-md shadow-md 
+    shadow-black rounded-md flex flex-col justify-center items-center gap-4 sm:gap-[20px] px-4 sm:px-[20px] py-6 sm:py-0' onSubmit={handleSignup}>
 
-      <h1 className='text-white text-3xl font-semibold mb-[30px]'>
+      <h1 className='text-white text-2xl sm:text-3xl font-semibold mb-4 sm:mb-[30px] text-center'>
         Register to  <span className='text-blue-400'>AI Virtual Assistant</span>
         </h1>
 
-        <input type="text" placeholder='Enter your Name' className='w-full h-[60px] outline-none
-         border-2 border-white bg-transparent text-white placeholder-gray-300 px-[20px] py-[10px] rounded-full text-[18px]'
+        <input type="text" placeholder='Enter your Name' className='w-full h-[50px] sm:h-[60px] outline-none
+         border-2 border-white bg-transparent text-white placeholder-gray-300 px-5 py-2 rounded-full text-[16px] sm:text-[18px]'
          required onChange={(e)=> setName(e.target.value)} value={name}/>
 
-         <input type="Email" placeholder='Email' className='w-full h-[60px] outline-none
-         border-2 border-white bg-transparent text-white placeholder-gray-300 px-[20px] py-[10px] rounded-full text-[18px]'
+         <input type="Email" placeholder='Email' className='w-full h-[50px] sm:h-[60px] outline-none
+         border-2 border-white bg-transparent text-white placeholder-gray-300 px-5 py-2 rounded-full text-[16px] sm:text-[18px]'
          required onChange={(e)=> setEmail(e.target.value)} value={email}/>
 
-         <div className='w-full h-[60px] border-2 border-white bg-transparent text-white rounded-full text-[18px]'>
+         <div className='w-full h-[60px] sm:h-[60px] border-2 border-white bg-transparent text-white rounded-full text-[18px]'>
 
           <input type="password" placeholder='Enter password' className='w-full h-full rounde-full outline-none bg-transparent
-          text-white placeholder-gray-300 px-[20px] py-[10px] text-[18px]'
+          text-white placeholder-gray-300 px-5 py-2 text-[16px] sm:text-[18px]'
           required onChange={(e)=> setPassword(e.target.value)} value={password}/>
          </div>
 
          {err.length>0 && <p className='text-red-500 text-[16px]'>*{err}</p>}
 
-         <button className='min-w-[140px] h-[50px] text-[18px] font-semibold bg-white text-black rounded-full mt-[30px]'disabled={loading}>{loading?"loading...":"Sign up"}</button>
+         <button className='min-w-[140px] h-[45px] sm:h-[50px] text-[16px] sm:text-[18px] font-semibold bg-white
+         text-black rounded-full mt-4 sm:mt-[30px]'disabled={loading}>{loading?"loading...":"Sign up"}</button>
 
-         <p className='text-white text-[16px] cursor-pointer' onClick={()=> navigate("/signin")}>Already have an account ? <span className='text-blue-400'>Sign in</span></p>
+         <p className='text-white text-sm sm:text-[16px] cursor-pointer' onClick={()=> navigate("/signin")}>Already have an account ? <span className='text-blue-400'>Sign in</span></p>
 
 
     </form>
