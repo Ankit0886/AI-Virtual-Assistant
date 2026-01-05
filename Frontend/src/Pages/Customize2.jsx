ac5d748924fa2bdb648b0a6ef3df4e03ec357913
Ankit0886 <ankitsinghchouhan0735@gmail.com>
2026-01-05 14:30:08 +0530

    Update Customize2.jsx

--- Frontend/src/Pages/Customize2.jsx
+++ Frontend/src/Pages/Customize2.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from 'react'
-import { userDataContext } from '../context/userContext'
+import { userDataContext } from '../context/UserContext'
 import { MdArrowBack } from "react-icons/md";
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom';
